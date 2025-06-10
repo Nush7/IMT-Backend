0e4a08a8037067ff0cee6a0673642dd506959131
Anushka <anushkasinghofficial2646@gmail.com>
2025-06-10 19:35:15 +0530

    Revert "feat: add Swagger API documentation and integrate with routes for authentication and product management"
    This reverts commit 7a326d4ef213bf5752e6463c67b2c94730dd7b55.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./src/config/database');
 const routes = require('./src/routes/index');
-const { swaggerUi, swaggerSpec } = require('./src/config/swagger');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -28,7 +27,6 @@ app.get('/health', (req, res) => {
 });
 
 app.use('/api', routes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
